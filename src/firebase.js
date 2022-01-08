6b9f968e3370efb717d0c76ed92b10986a85bf44
Bairamov Allaz <67458149+BairamovAllaz@users.noreply.github.com>
2022-01-08 18:54:15 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 const firebaseConfig = {
-  apiKey: "AIzaSyCWerd76gG7BKuCPILmnz9AC7D0_ykyef8",
-  authDomain: "whatsapp-7898f.firebaseapp.com",
-  projectId: "whatsapp-7898f",
-  storageBucket: "whatsapp-7898f.appspot.com",
-  messagingSenderId: "132891649375",
-  appId: "1:132891649375:web:035752c2b3a1854e10c2ea",
-  measurementId: "G-YHP71ZPWQ1"
+  ///firebase config
 };
 
 const fire = firebase.initializeApp(firebaseConfig)
